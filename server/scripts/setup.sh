1ee373d68971e4de80511f587478498d26dd481c
James Wang <jameswang9909@hotmail.com>
2016-05-17 20:05:21 -0400

    Add tmux to install script

--- server/scripts/setup.sh
+++ server/scripts/setup.sh
@@ -5,6 +5,6 @@ MYSQL_ROOT_PASSWORD=password
 sudo apt-get -y update
 
 sudo apt-get -y install python python-pip python-dev
-sudo apt-get -y install sqlite3
+sudo apt-get -y install sqlite3 tmux
 
 sudo pip install -r requirements.txt
